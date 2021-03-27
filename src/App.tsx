dd5465a3c929809623330e043029051735c3d6ec
SeniyaS <seniyas@lolctech.com>
2021-03-27 22:13:53 +0530

    Changes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,22 @@
 import React from "react";
 import logo from "./logo.svg";
-import { Breadcrumb, DatePicker } from "antd";
+import { Breadcrumb, DatePicker, Input } from "antd";
 import { HomeOutlined, UserOutlined } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./store/storeConfig";
 import "./App.less";
 import "./App.css";
+import Layout, { Content } from "antd/lib/layout/layout";
 
 function App() {
   const dispatch = useDispatch();
   const isLoading = useSelector((state: RootState) => state.isLoading);
   return (
-    <div>
-      <Breadcrumb className='m-3 text-red-400'>
-        <Breadcrumb.Item href=''>
-          <HomeOutlined />
-        </Breadcrumb.Item>
-        <Breadcrumb.Item href=''>
-          <UserOutlined />
-          <span className='text-red-600'>Is Loading {isLoading}</span>
-        </Breadcrumb.Item>
-        <Breadcrumb.Item>Application</Breadcrumb.Item>
-      </Breadcrumb>
-      <DatePicker></DatePicker>
-    </div>
+    <Layout>
+      <Content className="w-50 p-10">
+        <Input type="text"></Input>
+      </Content>
+    </Layout>
   );
 }
 
