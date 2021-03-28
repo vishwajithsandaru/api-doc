68d8ba76757e7547f2ee2790a6f3fa90014c6cd6
SeniyaS <seniyadissanayake@gmail.com>
2021-03-28 09:12:23 +0530

    final object

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,23 @@ import { Definitions, Paths, SwaggerType } from "./models/swagger";
 export interface TreeItem {
   title: string;
   key: string;
-  checkable?:boolean,
+  checkable?: boolean;
   children?: TreeItem[];
 }
 function App() {
-  const [endPointTreeData, setEndPointTreeData] = useState<TreeItem[] | null>(null);
-  const [definitionTreeData, setDefinitionTreeData] = useState<TreeItem[] | null>(null);
+  const [endPointTreeData, setEndPointTreeData] = useState<TreeItem[] | null>(
+    null
+  );
+  const [definitionTreeData, setDefinitionTreeData] = useState<
+    TreeItem[] | null
+  >(null);
 
   const [jsonObject, setJsonObject] = useState<SwaggerType | null>(null);
-  const [selectedPaths,setSelectedPaths] = useState<Paths|null>(null);
-  const [selectedDefinitions,setSelectedDefinitions] = useState<Definitions|null>(null);
+  const [selectedPaths, setSelectedPaths] = useState<Paths | null>(null);
+  const [
+    selectedDefinitions,
+    setSelectedDefinitions,
+  ] = useState<Definitions | null>(null);
 
   useEffect(() => {
     let options = { codeSamples: true };
@@ -38,23 +45,27 @@ function App() {
     doThis();
 
     let endPointTree: TreeItem[] = [];
-    let definitionTree :TreeItem[]=[];
+    let definitionTree: TreeItem[] = [];
     if (jsonObject) {
       for (const [pathKey, path] of Object.entries(jsonObject.paths)) {
         let itemChild: TreeItem[] = [];
         for (const [key, leaf] of Object.entries(path)) {
           itemChild.push({ key: `${pathKey}///${key}`, title: key });
         }
-        endPointTree.push({ key: pathKey, title: pathKey, children: itemChild });
+        endPointTree.push({
+          key: pathKey,
+          title: pathKey,
+          children: itemChild,
+        });
       }
 
       console.log(jsonObject);
-      if(jsonObject.definitions){
-        for (const [definitionKey, value] of Object.entries(jsonObject.definitions)) {
-          definitionTree.push({key:definitionKey,title:definitionKey});
+      if (jsonObject.definitions) {
+        for (const [definitionKey, value] of Object.entries(
+          jsonObject.definitions
+        )) {
+          definitionTree.push({ key: definitionKey, title: definitionKey });
         }
-        
-        
       }
       setDefinitionTreeData(definitionTree);
       setEndPointTreeData(endPointTree);
@@ -70,32 +81,33 @@ function App() {
     }
   }, [jsonObject]);
 
-
-
   const onEndPointCheck = (checkedkeys: any) => {
     console.log(checkedkeys);
 
-    let selectedTemp:Paths={};
+    let selectedTemp: Paths = {};
     for (const key of checkedkeys) {
-      const [basePath,method] = (key as string).split('///');
-      if(method){
-        selectedTemp={...selectedTemp,[basePath]:{
-          ...selectedTemp[basePath],
-          [method]:{...(jsonObject?.paths[basePath][method])}
-          }}
+      const [basePath, method] = (key as string).split("///");
+      if (method) {
+        selectedTemp = {
+          ...selectedTemp,
+          [basePath]: {
+            ...selectedTemp[basePath],
+            [method]: { ...jsonObject?.paths[basePath][method] },
+          },
+        };
       }
     }
 
-    setSelectedPaths(selectedTemp)
+    setSelectedPaths(selectedTemp);
   };
 
-  const onDefinitionCheck=(checkedKey:any)=>{
+  const onDefinitionCheck = (checkedKey: any) => {
     let tempSelected = {};
-    for (const key of checkedKey ) {
-        tempSelected={...tempSelected,[key]:jsonObject?.definitions[key]}
+    for (const key of checkedKey) {
+      tempSelected = { ...tempSelected, [key]: jsonObject?.definitions[key] };
     }
     setSelectedDefinitions(tempSelected);
-  }
+  };
 
   const fileReader = new FileReader();
 
@@ -120,7 +132,6 @@ function App() {
         <Input type="text"></Input>
 
         <Content>
-        
           {endPointTreeData && (
             <Tree
               // checkStrictly
@@ -129,22 +140,25 @@ function App() {
               onCheck={onEndPointCheck}
             ></Tree>
           )}
-
-
-</Content>
-<Content className="p-10">
-          {
-            definitionTreeData && (
-              <Tree
+        </Content>
+        <Content className="p-10">
+          {definitionTreeData && (
+            <Tree
               // checkStrictly
               checkable
               treeData={definitionTreeData}
               onCheck={onDefinitionCheck}
             ></Tree>
-            )
-          }
-
-</Content>
+          )}
+        </Content>
+        {/* create final object */}
+        <Content className="p-10">
+          {JSON.stringify({
+            ...jsonObject,
+            paths: selectedPaths,
+            definitions: selectedDefinitions,
+          })}
+        </Content>
       </Content>
     </Layout>
   );
